7316f558862a95b394f39d87c77be02121e6b526
Nicolas Schodet <nico@ni.fr.eu.org>
2021-09-30 00:40:29 +0200

    Replace the build system with Meson
    The SCons based build was broken using the current version. It needs to
    be fixed or replaced.
    
    SCons requires too much local code to make it work correctly, as said on
    https://wiki.debian.org/UpstreamGuide#SCons, or
    https://wiki.gentoo.org/wiki/SCons#Why_you_should_NOT_use_SCons_in_your_project
    
    Change the build to use Meson. At the same time:
    
    - remove the download of flash.bin from libnxt.googlecode.com as it does
      not work anymore,
    - simplify the header generation,
    - update the README file with new instructions and URLs.

--- make_flash_header.py
+++ make_flash_header.py
@@ -1,105 +1,61 @@
 #!/usr/bin/env python3
 #
-# Take the flash_routine.bin file, and embed it as an array of bytes
-# in a flash_routine.h, ready for packaging with the C firmware
-# flasher.
+"""Embed flash routine as an array of bytes in a header file."""
 #
-# If a file name is provided on the commandline, load that file as the
-# firmware flashing routine instead.
+# Copyright 2006 David Anderson <david.anderson@calixo.net>
 #
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
 
+import argparse
 import sys
-import os
-import os.path
-import urllib.request, urllib.error, urllib.parse
-import hashlib
 
-FLASH_DIR = 'flash_write'
-FLASH_BIN = 'flash.bin'
-FLASH_PATH = os.path.join(FLASH_DIR, FLASH_BIN)
+p = argparse.ArgumentParser(description=__doc__)
+p.add_argument('binary',
+               help='flash routine binary file')
+p.add_argument('template',
+               help='header template')
+p.add_argument('-o', '--output', metavar='output', required=True,
+               help='output header')
+options = p.parse_args()
 
-DOWNLOAD_FLASH_CHECKSUM = '589501072d76be483f873a787080adcab20841f4'
-DOWNLOAD_FLASH_URL = 'http://libnxt.googlecode.com/files/flash.bin'
+# Read the binary
+with open(options.binary, 'rb') as f:
+    fwbin = f.read()
 
-def check_flash_size():
-    statinfo = os.stat(FLASH_PATH)
-    if statinfo.st_size > 1024:
-        print("The flash driver looks too big, refusing to embed.")
-        return False
-    return True
+if len(fwbin) > 1024:
+    print("The flash driver looks too big, refusing to embed.",
+          file=sys.stderr)
+    sys.exit(1)
 
-def ensure_flash_bin():
-    # If the flash binary does exist, just check its size and return.
-    if os.path.isfile(FLASH_PATH):
-        return check_flash_size()
+data = [f'0x{c:02x}' for c in fwbin]
 
-    # If the binary doesn't exist, offer to download a binary build
-    # from Google Code.
-    print("""
-Embedded flash driver not found. This is required to build LibNXT.
+for i in range(0, len(data), 12):
+    data[i] = "\n  " + data[i]
 
-If you have an ARM7 cross-compiler toolchain available, you can build
-the flash driver by interrupting (ctrl-C) this build and running
-'make' in the 'flash_write' subdirectory. Then rerun this build again,
-and everything should work great.
+data_str = ', '.join(data)
+len_data = "%d" % len(data)
 
-If you do not have a cross-compiler, do not despair! I can also
-download a copy of the compiled driver (built by the libnxt developer)
-from the project website and use that.
-""")
-    reply = input("Is that okay? (y/n) ")
-    if reply not in ('y', 'Y', 'yes'):
-        print ("Okay, you're the boss. But that does mean I can't build "
-               "LibNXT. Sorry.")
-        return False
-    f = urllib.request.urlopen(DOWNLOAD_FLASH_URL)
-    data = f.read()
-    f.close()
+# Read in the template
+with open(options.template, 'rt') as tplfile:
+    template = tplfile.read()
 
-    # Verify the SHA-1 checksum
-    checksum = hashlib.sha1(data).hexdigest()
-    if checksum != DOWNLOAD_FLASH_CHECKSUM:
-        print("Oops, the flash binary I downloaded has the wrong checksum!")
-        print("Aborting :(")
-        return False
+# Replace the values in the template
+template = template.replace('___FLASH_BIN___', data_str + '\n')
+template = template.replace('___FLASH_LEN___', len_data)
 
-    with open(FLASH_PATH, 'wb') as f:
-        f.write(data)
-
-    if os.path.isfile(FLASH_PATH):
-        return check_flash_size()
-
-    # Dude, you're really not lucky, are you.
-    return False
-
-
-def main():
-    if not ensure_flash_bin():
-        sys.exit(1)
-
-    with open(FLASH_PATH, 'rb') as f:
-        fwbin = f.read()
-
-    data = [f'0x{c:02x}' for c in fwbin]
-
-    for i in range(0, len(data), 12):
-        data[i] = "\n  " + data[i]
-
-    data_str = ', '.join(data)
-    len_data = "%d" % len(data)
-
-    # Read in the template
-    with open('flash_routine.h.base', 'rt') as tplfile:
-        template = tplfile.read()
-
-    # Replace the values in the template
-    template = template.replace('___FLASH_BIN___', data_str + '\n')
-    template = template.replace('___FLASH_LEN___', len_data)
-
-    # Output the done header
-    with open('flash_routine.h', 'wt') as out:
-        out.write(template)
-
-if __name__ == '__main__':
-    main()
-
+# Output the done header
+with open(options.output, 'wt') as out:
+    out.write(template)
